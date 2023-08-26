561cba47572b06e8b5e7916bc5c8bacd316b1f96
saro-dev <codersaro@gmail.com>
2023-08-26 18:05:14 +0530

    first commit

---
+++ src/components/Cart/CartIcon.js
@@ -0,0 +1,9 @@
+const CartIcon = () => {
+
+  return (
+    <div className="cart-icon">
+      <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/shopping-cart--v1.png" alt="shopping-cart--v1"/>
+    </div>
+  );
+}
+export default CartIcon;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import {Fragment} from 'react';
+import Image from '../../Assets/back-customers.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+const Header=props=>{
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <HeaderCartButton onClick={props.onShowCart} />
+        </header>
+        <div className={classes['main-image']}>
+            <img src={Image} alt='meal' />
+        </div>
+    </Fragment>
+}
+export default Header;
