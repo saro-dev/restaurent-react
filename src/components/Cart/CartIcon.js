0c496ef08709f8d5b2a09cacc94e083c708a96f9
saro-dev <codersaro@gmail.com>
2023-08-27 08:49:03 +0530

    second commit

--- src/components/Cart/CartIcon.js
+++ src/components/Cart/CartIcon.js
@@ -1,8 +1,9 @@
+import Cartpng from './icons8-cart-90.png'
 const CartIcon = () => {
 
   return (
     <div className="cart-icon">
-      <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/shopping-cart--v1.png" alt="shopping-cart--v1"/>
+      <img width="30" height="30" src={Cartpng} alt='cart icon'/>
     </div>
   );
 }
